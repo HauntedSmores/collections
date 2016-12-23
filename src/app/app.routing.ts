e5138872e555ea324d8754ae07363fc491090d58
robachma <robertest16@gmail.com>
2016-12-22 20:03:25 -0800

    adding dashboard component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,12 @@
 import {Routes,RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {LandingComponentComponent} from './landing-component/landing-component.component';
+import {DashboardComponent} from './dashboard/dashboard.component'
 
  const routes:Routes = [
      {path:'login',component:LoginComponent},     
-     {path:'',component: LandingComponentComponent}
+     {path:'',component: LandingComponentComponent},
+     {path:'dashboard',component: DashboardComponent}
 
  ];
  export const routing = RouterModule.forRoot(routes);
